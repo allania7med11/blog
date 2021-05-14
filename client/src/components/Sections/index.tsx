0b68948933a44d32006c88d3747b7d05fa8e1f3d
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-05-14 04:58:33 +0100

    hooks

--- client/src/components/Sections/index.tsx
+++ client/src/components/Sections/index.tsx
@@ -1,7 +1,7 @@
 import btn from "@/assets/css/btn.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 import { currentCreate, extendUpdate } from "@/actions/section";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,13 +10,12 @@ import Form from "./Form";
 import Cards from "./Cards";
 import Sidebar from "./Sidebar";
 import { upUpdate } from "@/actions/breakpoint";
-import { debounce, getBreakpointUp } from "@/hooks/useBreakpoint";
+import {  useBreakpoint } from "@/hooks/useBreakpoint";
 
 enum CSS {
   shadow = "z-10 w-screen h-screen fixed top-0 left-0 bg-gray-500 opacity-25",
 }
 const Sections: FC = () => {
-
   let { id } = useParams<{ id: string }>();
   const [show, margin, shadow] = useSelector((state: RootState) => {
     const { show, extend } = state.section;
@@ -26,23 +25,18 @@ const Sections: FC = () => {
     return [show, margin, shadow] as const;
   });
   const dispatch = useDispatch();
-  useEffect(() => {
-    let BreakpointUp = getBreakpointUp(window.innerWidth)
-    dispatch(upUpdate(BreakpointUp))
-    if(!BreakpointUp.sm){
-      dispatch(extendUpdate())
-    }
-    const calcInnerWidth = debounce(() => {
-      let BreakpointUp = getBreakpointUp(window.innerWidth)
-      dispatch(upUpdate(BreakpointUp))
-    }, 500);
-    window.addEventListener("resize", calcInnerWidth);
-    return () => window.removeEventListener("resize", calcInnerWidth);
-  }, [dispatch]);
+  useBreakpoint((payload: { [key: string]: boolean }) =>
+    dispatch(upUpdate(payload))
+  );
   return (
     <div>
       <Sidebar />
-      {shadow && <div className={CSS.shadow} onClick={() => dispatch(extendUpdate())}></div>}
+      {shadow && (
+        <div
+          className={CSS.shadow}
+          onClick={() => dispatch(extendUpdate())}
+        ></div>
+      )}
       {show && <Form />}
       <div className={margin}>
         <button
--- client/src/hooks/useBreakpoint.ts
+++ client/src/hooks/useBreakpoint.ts
@@ -1,4 +1,5 @@
 
+import { useEffect } from "react";
 import tailwindConfig from "../tailwind.config";
 
 
@@ -21,13 +22,15 @@ export const debounce = (fn:Function, wait:number) => {
     time = setTimeout(() => fn(...args), wait);
   };
 };
-// const useBreakpoint = (update) => {
-//   useEffect(() => {
-//     const calcInnerWidth = debounce(() => {
-//       update(getBreakpointUp(window.innerWidth));
-//     }, 500);
-//     window.addEventListener("resize", calcInnerWidth);
-//     return () => window.removeEventListener("resize", calcInnerWidth);
-//   });
-// };
+export const useBreakpoint = (update:Function) => {
+  useEffect(() => {
+    update(getBreakpointUp(window.innerWidth))
+    const calcInnerWidth = debounce(() => {
+      console.log(window.innerWidth)
+      update(getBreakpointUp(window.innerWidth));
+    }, 500);
+    window.addEventListener("resize", calcInnerWidth);
+    return () => window.removeEventListener("resize", calcInnerWidth);
+  },[]);
+};
 
