933806d9933b303510db5e4977103646b4e6c0f5
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-05-14 05:47:31 +0100

    sidebar top

--- client/src/components/Sections/Sidebar/index.tsx
+++ client/src/components/Sections/Sidebar/index.tsx
@@ -8,7 +8,7 @@ import Parent from "./Parent";
 import Child from "./Child";
 import { extendUpdate } from "@/store/section/actions";
 enum CSS {
-    navbar = "z-20 text-left h-screen text-lg font-medium truncate bg-gray-800 whitespace-nowrap fixed top-12 left-0 flex flex-col pl-4 pb-4 pt-2   text-gray-400"
+    navbar = "z-20 text-left h-screen text-lg font-medium truncate bg-gray-800 whitespace-nowrap fixed top-14 left-0 flex flex-col pl-4 pb-4 pt-2   text-gray-400"
 }
 const useExtend = (extend: boolean) => {
     const dispatch = useDispatch();
