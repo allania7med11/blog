add97ec90701a0345996a71261be04d6c42c3364
allania7med11 <allania7med11@gmail.com>
2021-07-18 13:13:32 +0100

    production

--- server/app.js
+++ server/app.js
@@ -1,16 +1,9 @@
 const cors = require("cors");
 var express = require('express');
 var app = express();
-if (process.env.NODE_ENV !== "production") {
-  app.use(cors());
-}
-// for debugging
-app.use("",(req,res,next) => {
-  next()
-})
-if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "production") {
-  app.use(express.static('../client/build'))
-} 
+
+
+app.use(express.static('../client/build'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(require('./routes'));
