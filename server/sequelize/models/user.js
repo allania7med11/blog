5e7caaf589e098c89ec92fa9e9242e65384cead3
allania7med11 <allania7med11@gmail.com>
2021-07-20 16:57:44 +0100

    user model

--- server/sequelize/models/user.js
+++ server/sequelize/models/user.js
@@ -4,12 +4,20 @@ module.exports = (sequelize) => {
   sequelize.define("user", {
     username: {
       type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false
     }
   });
 };
