2d7840ea3c1cf750befb15bc9c420988395d8387
allania7med11 <allania7med11@gmail.com>
2021-07-20 23:42:48 +0100

    create user

--- client/src/components/USERS/Create.tsx
+++ client/src/components/USERS/Create.tsx
@@ -1,15 +1,22 @@
-import react from "react"
+import react, { FormEvent } from "react"
 import formCss from "@/assets/css/form.module.scss";
 import cardCss from "@/assets/css/card.module.scss";
 import btnCss from "@/assets/css/btn.module.scss"
 import { useInput } from "@/hooks/useInput";
+import { apiUser } from "@/store/user/api";
 export const CreateUser = () => {
     const [username, changeUsername] = useInput("");
     const [email, changeEmail] = useInput("");
     const [password, changePassword] = useInput("");
+    const submit = async (evt: FormEvent<HTMLFormElement>) => {
+        evt.preventDefault()
+        let response = await apiUser.create({ username, email, password })
+        console.log(response.data);
+        
+    }
     return (
         <div className={`${cardCss.card} mt-16 max-w-xl z-10 font-serif`}>
-            <form className={formCss.form}>
+            <form className={formCss.form} onSubmit={(evt) => submit(evt)}>
                 <div className={formCss.title}>Sign Up</div>
                 <label htmlFor="fusername">Username:</label>
                 <input
---
+++ client/src/store/user/types.ts
@@ -0,0 +1,8 @@
+export interface UserCreate {
+  username: string;
+  password: string;
+  email: string;
+}
+
+
+
